311c307ecbea72627b5f5ee09286e5c23187da03
engks4619 <engks4619@naver.com>
2022-03-10 23:27:00 +0900

    DP

--- baekjoon/Boj_10844_쉬운_계단_수.java
+++ baekjoon/Boj_10844_쉬운_계단_수.java
@@ -15,9 +15,9 @@ public class Boj_10844_쉬운_계단_수 {
 		for (int i = 2; i <= N; i++) {
 			for (int j = 0; j < 10; j++) {
 				if (j == 0)
-					dp[i][0] = dp[i - 1][1];
+					dp[i][j] = dp[i - 1][1];
 				else if (j == 9)
-					dp[i][9] = dp[i - 1][8];
+					dp[i][j] = dp[i - 1][8];
 				else
 					dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j + 1];
 			}
