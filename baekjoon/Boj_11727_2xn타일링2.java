4e7938523fc1b413afed83c4b3e24e90a2d5316b
engks4619 <engks4619@naver.com>
2022-04-17 22:19:55 +0900

    DP

---
+++ baekjoon/Boj_11727_2xn타일링2.java
@@ -0,0 +1,21 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Boj_11727_2xn타일링2 {
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(in.readLine());
+		int dp[] = new int[N+1];
+		dp[1] = 1;
+		dp[2] = 3;
+		for(int i = 3; i <= N; i++) {
+			dp[i] = (2*dp[i-2] + dp[i-1]) % 10007;
+		}
+		System.out.println(dp[N]);
+		in.close();
+	}
+	
+}
