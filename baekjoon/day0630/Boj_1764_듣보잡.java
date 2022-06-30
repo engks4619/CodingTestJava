9a4d013f555b538dd8a5fdf577362462c8223a58
engks4619 <engks4619@naver.com>
2022-07-01 00:18:40 +0900

    HashSet, PQ

--- baekjoon/day0630/Boj_1764_듣보잡.java
+++ baekjoon/day0630/Boj_1764_듣보잡.java
@@ -2,10 +2,7 @@ package baekjoon.day0630;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashSet;
-import java.util.List;
 import java.util.PriorityQueue;
 import java.util.Set;
 import java.util.StringTokenizer;
