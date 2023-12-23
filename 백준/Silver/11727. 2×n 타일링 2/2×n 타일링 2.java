463158f60e970aa9b2a0d3c6f51f92538614732f
Yechan_Lee <57943574+engks4619@users.noreply.github.com>
2023-12-24 07:11:47 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 76 ms, Memory: 11512 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(in.readLine());
+		int dp[] = new int[N+1];
+		if(N <= 1) {
+			System.out.println(N);
+			in.close();
+			return;
+		}
+		dp[1] = 1;
+		dp[2] = 3;
+		for(int i = 3; i <= N; i++) {
+			dp[i] = (2*dp[i-2] + dp[i-1]) % 10007;
+		}
+		System.out.println(dp[N]);
+		in.close();
+	}
+}
