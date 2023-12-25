e4b753ebde511d5efec941037951b0e609286819
Yechan_Lee <57943574+engks4619@users.noreply.github.com>
2023-12-26 07:01:48 +0900

    [Silver III] Title: 이친수, Time: 76 ms, Memory: 11488 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.java
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -5,13 +5,13 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(in.readLine());
-        long[] dp = new long[91];
-        dp[1] = 1;
-        dp[2] = 1;
-        for (int i = 3; i <= N; i++) {
-            dp[i] = dp[i-1] + dp[i-2];
+        long[][] dp = new long[91][2];
+        dp[1][1] = 1;
+        for (int i = 2; i <= N; i++) {
+            dp[i][0] = dp[i-1][0] + dp[i-1][1];
+            dp[i][1] = dp[i-1][0];
         }
-        System.out.println(dp[N]);
+        System.out.println(dp[N][0] + dp[N][1]);
         in.close();
     }
 }
