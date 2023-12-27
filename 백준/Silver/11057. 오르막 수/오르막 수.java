7d9f88599dcd6bfa162343999a40f9f3b7d28919
Yechan_Lee <57943574+engks4619@users.noreply.github.com>
2023-12-27 23:32:59 +0900

    [Silver I] Title: 오르막 수, Time: 76 ms, Memory: 11624 KB -BaekjoonHub

---
+++ 백준/Silver/11057. 오르막 수/오르막 수.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(in.readLine());
+		long[][] dp = new long[1001][10];
+		for(int i = 0 ; i <= 9; i++) {
+			dp[1][i] = 1;
+		}
+		for(int i = 2; i <= N; i++) {
+			for(int j = 0; j < 10; j++) {
+				for(int k = 0; k <= j; k++) {
+					dp[i][j] += dp[i-1][k] % 10007;					
+				}
+			}
+		}
+		long result = 0;
+		for(int i = 0; i <= 9; i++) {
+			result += dp[N][i] % 10007;
+		}
+		System.out.println(result % 10007);
+		in.close();
+	}
+}
