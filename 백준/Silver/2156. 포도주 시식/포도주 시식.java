55ffa75f71aa87d4df07d03d8ee904aa2681ecb1
Yechan_Lee <57943574+engks4619@users.noreply.github.com>
2023-12-28 06:57:09 +0900

    [Silver I] Title: 포도주 시식, Time: 108 ms, Memory: 13296 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.java
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -6,18 +6,15 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(in.readLine());
-        int[] arr = new int[N];
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(in.readLine());
-        }
         int[][] dp = new int[10001][3];
-        dp[0][1] = arr[0];
+        dp[0][1] = Integer.parseInt(in.readLine());
         for (int i = 1; i < N; i++) {
+            int num = Integer.parseInt(in.readLine());
             dp[i][0] = Math.max(Math.max(dp[i - 1][0], dp[i - 1][1]), dp[i - 1][2]);
-            dp[i][1] = dp[i - 1][0] + arr[i];
-            dp[i][2] = dp[i - 1][1] + arr[i];
+            dp[i][1] = dp[i - 1][0] + num;
+            dp[i][2] = dp[i - 1][1] + num;
         }
-        System.out.println(Arrays.stream(dp[N-1]).max().getAsInt());
+        System.out.println(Math.max(Math.max(dp[N - 1][0], dp[N - 1][1]), dp[N - 1][2]));
         in.close();
     }
 }
