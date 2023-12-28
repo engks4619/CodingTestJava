3a9763e88c3ddf469e2b86be2d00020a1b497cae
Yechan_Lee <57943574+engks4619@users.noreply.github.com>
2023-12-29 00:31:56 +0900

    [Silver I] Title: 정수 삼각형, Time: 220 ms, Memory: 23452 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -1,6 +1,5 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -8,7 +7,6 @@ public class Main {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(in.readLine());
         int[][] arr = new int[N][N];
-        int[][] dp = new int[N][N];
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(in.readLine());
             for (int j = 0; j < i + 1; j++) {
@@ -16,21 +14,12 @@ public class Main {
             }
 
         }
-        dp[0][0] = arr[0][0];
-        for (int i = 1; i < N; i++) {
+        for (int i = N - 2; i >= 0; i--) {
             for (int j = 0; j < i + 1; j++) {
-                if(j == 0){
-                    dp[i][j] = dp[i - 1][j] + arr[i][j];
-                    continue;
-                }
-                if(j == i){
-                    dp[i][j] = dp[i - 1][j - 1] + arr[i][j];
-                    continue;
-                }
-                dp[i][j] = Math.max(dp[i - 1][j - 1], dp[i - 1][j]) + arr[i][j];
+                arr[i][j] += Math.max(arr[i + 1][j], arr[i + 1][j + 1]);
             }
         }
-        System.out.println(Arrays.stream(dp[N-1]).max().getAsInt());
+        System.out.println(arr[0][0]);
         in.close();
     }
 }
