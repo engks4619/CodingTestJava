2508b57ed2b41b66cde9f4f37b672c3b0347a4c4
Yechan_Lee <57943574+engks4619@users.noreply.github.com>
2024-01-01 04:46:55 +0900

    [Silver II] Title: N과 M (11), Time: 312 ms, Memory: 45176 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static int N, M;
+	static int[] selected;
+	static Integer[] data;
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(in.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(in.readLine());
+		Set<Integer> set = new HashSet<>();
+		for(int i = 0; i < N; i++) {
+			set.add(Integer.parseInt(st.nextToken()));
+		}
+		N = set.size();
+		data = set.toArray(new Integer[N]);
+		Arrays.sort(data);
+		selected = new int[M];
+		dupPermutation(0);
+		System.out.println(sb.toString());
+		in.close();
+	}
+	
+	static void dupPermutation(int cnt) {
+		if(cnt == M) {
+			for(int i = 0; i < M; i++) {
+				sb.append(selected[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		for(int i = 0; i < N; i++) {
+			selected[cnt] = data[i];
+			dupPermutation(cnt+1);
+		}
+	}
+	
+}
