184a0550112d8b3deebdbe44278803ac4148c0d3
Yechan_Lee <57943574+engks4619@users.noreply.github.com>
2023-12-31 21:52:39 +0900

    [Silver III] Title: N과 M (8), Time: 124 ms, Memory: 16372 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static int[] arr, selected;
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        sb = new StringBuilder();
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        selected = new int[M];
+        st = new StringTokenizer(in.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        go(0, 0);
+        System.out.print(sb);
+        in.close();
+    }
+
+    static boolean go(int idx, int cnt){
+        if(cnt == M){
+            for(int num : selected){
+                sb.append(num).append(" ");
+            }
+            sb.append("\n");
+            return true;
+        }
+        for (int i = idx; i < N; i++) {
+            selected[cnt] = arr[i];
+            go(i, cnt + 1);
+        }
+        return false;
+    }
+}
