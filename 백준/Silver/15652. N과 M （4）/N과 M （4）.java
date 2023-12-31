61746be78267058ba12995f174c0d69ccf9b64f5
Yechan_Lee <57943574+engks4619@users.noreply.github.com>
2023-12-31 09:56:12 +0900

    [Silver III] Title: N과 M (4), Time: 100 ms, Memory: 13936 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static int[] selected;
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        selected = new int[M];
+        permutation( 1, 0);
+        System.out.print(sb);
+        in.close();
+    }
+
+    static void permutation(int curr, int cnt){
+        if(cnt == M){
+            for (int num : selected) {
+                sb.append(num).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = curr; i <= N; i++) {
+            selected[cnt] = i;
+            permutation(i, cnt + 1);
+        }
+    }
+}
