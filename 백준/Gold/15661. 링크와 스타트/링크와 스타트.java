bb81fa800a01b995522d5b392ad9b217d74a69ba
Yechan_Lee <57943574+engks4619@users.noreply.github.com>
2024-01-02 07:33:28 +0900

    [Gold V] Title: 링크와 스타트, Time: 312 ms, Memory: 17640 KB -BaekjoonHub

--- 백준/Gold/15661. 링크와 스타트/링크와 스타트.java
+++ 백준/Gold/15661. 링크와 스타트/링크와 스타트.java
@@ -2,60 +2,55 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
 
     static int N;
-    static int[][] scoreBoard;
     static int minScoreDiff = Integer.MAX_VALUE;
+    static int[] teamA, teamB;
+    static int[][] scoreBoard;
 
     public static void main(String[] args) throws Exception {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         N = Integer.parseInt(in.readLine());
         scoreBoard = new int[N][N];
+        teamA = new int[N];
+        teamB = new int[N];
         StringTokenizer st;
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(in.readLine());
+            st= new StringTokenizer(in.readLine());
             for (int j = 0; j < N; j++) {
                 scoreBoard[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        combination(0, 0, new boolean[N]);
+        go(0, 0, 0, 0, 0);
         bw.write(minScoreDiff + "\n");
         bw.flush();
         bw.close();
         in.close();
     }
 
-    static void combination(int idx, int cnt, boolean[] selected){
-        if(cnt >= N) return;
-        if(cnt > 0){
-            List<Integer> teamStart = new ArrayList<>(N / 2);
-            List<Integer> teamLink = new ArrayList<>(N / 2);
-            for (int i = 0; i < N; i++) {
-                if(selected[i]) teamStart.add(i);
-                else teamLink.add(i);
-            }
-            minScoreDiff = Math.min(minScoreDiff, Math.abs(getScore(teamStart) - getScore(teamLink)));
+    static void go(int cnt, int aCnt, int bCnt, int aScore, int bScore){
+        if(cnt == N){
+            if(aCnt == 0 || bCnt == 0) return;
+            minScoreDiff = Math.min(minScoreDiff, Math.abs(aScore - bScore));
+            return;
         }
-        for (int i = idx; i < N; i++) {
-            selected[i] = true;
-            combination(i + 1,cnt + 1, selected);
-            selected[i] = false;
-        }
-    }
 
-    static int getScore(List<Integer> team){
         int score = 0;
-        for (int i = 0; i < team.size(); i++) {
-            for (int j = 0; j < team.size(); j++) {
-                score += scoreBoard[team.get(i)][team.get(j)];
-            }
+        for (int i = 0; i < aCnt; i++) {
+            score += scoreBoard[teamA[i]][cnt] + scoreBoard[cnt][teamA[i]];
         }
-        return score;
+        teamA[aCnt] = cnt;
+        go(cnt + 1, aCnt + 1, bCnt, aScore + score, bScore);
+
+        score = 0;
+        for (int i = 0; i < bCnt; i++) {
+            score += scoreBoard[teamB[i]][cnt] + scoreBoard[cnt][teamB[i]];
+        }
+        teamB[bCnt] = cnt;
+        go(cnt + 1, aCnt, bCnt + 1, aScore, bScore + score);
     }
 }
