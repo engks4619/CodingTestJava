ae5ea56e97c09a0ff9271adb6e62f52ca66bf587
Yechan_Lee <57943574+engks4619@users.noreply.github.com>
2024-01-02 06:51:30 +0900

    [Silver I] Title: 스타트와 링크, Time: 484 ms, Memory: 45476 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -1,82 +1,61 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N;
-	static int min;
-	static int[][] synergy;
-	static boolean[] isSelected;
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		min = Integer.MAX_VALUE;
-		N = Integer.parseInt(in.readLine());
-		synergy = new int[N][N];
+    static int N;
+    static int[][] scoreBoard;
+    static int minScoreDiff = Integer.MAX_VALUE;
 
-		for (int i = 0; i < N; i++) {
-			StringTokenizer st = new StringTokenizer(in.readLine());
-			for (int j = 0; j < N; j++) {
-				synergy[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		int[] p = new int[N];
-		int cnt = 0;
-		while (++cnt <= N / 2)
-			p[N - cnt] = 1;
+    public static void main(String[] args) throws Exception {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        N = Integer.parseInt(in.readLine());
+        scoreBoard = new int[N][N];
+        StringTokenizer st;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(in.readLine());
+            for (int j = 0; j < N; j++) {
+                scoreBoard[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        combination(0, 0, new boolean[N]);
+        bw.write(minScoreDiff + "\n");
+        bw.flush();
+        bw.close();
+        in.close();
+    }
 
-		do {
-			List<Integer> materialA = new ArrayList<Integer>(N / 2);
-			List<Integer> materialB = new ArrayList<Integer>(N / 2);
-			for (int i = 0; i < N; i++) {
-				if (p[i] == 1)
-					materialA.add(i);
-				else
-					materialB.add(i);
-			}
-			int tasteA = getTasteScore(materialA);
-			int tasteB = getTasteScore(materialB);
-			min = Math.min(min, Math.abs(tasteA - tasteB));
-		} while (np(p));
-		System.out.println(min);
-		in.close();
-	}
+    static void combination(int idx, int cnt, boolean[] selected){
+        if(cnt == N / 2){
+            List<Integer> teamStart = new ArrayList<>(N / 2);
+            List<Integer> teamLink = new ArrayList<>(N / 2);
+            for (int i = 0; i < N; i++) {
+                if(selected[i]) teamStart.add(i);
+                else teamLink.add(i);
+            }
+            minScoreDiff = Math.min(minScoreDiff, Math.abs(getScore(teamStart) - getScore(teamLink)));
+            return;
+        }
+        for (int i = idx; i < N; i++) {
+           selected[i] = true;
+           combination(i + 1,cnt + 1, selected);
+           selected[i] = false;
+        }
+    }
 
-	private static boolean np(int[] p) {
-		int N = p.length;
-		int i = N - 1;
-		while (i > 0 && p[i - 1] >= p[i])
-			--i;
-		if (i == 0)
-			return false;
-		int j = N - 1;
-		while (p[i - 1] >= p[j])
-			--j;
-		swap(p, i - 1, j);
-		int k = N - 1;
-		while (i < k) {
-			swap(p, i++, k--);
-		}
-		return true;
-	}
-
-	public static void swap(int[] p, int i, int j) {
-		int temp = p[i];
-		p[i] = p[j];
-		p[j] = temp;
-	}
-
-	static int getTasteScore(List<Integer> material) {
-		int tasteScore = 0;
-		for (int i = 0; i < material.size() - 1; i++) {
-			for (int j = i + 1; j < material.size(); j++) {
-				int I = material.get(i);
-				int J = material.get(j);
-				tasteScore += synergy[I][J] + synergy[J][I];
-			}
-		}
-		return tasteScore;
-	}
+    static int getScore(List<Integer> team){
+        int score = 0;
+        for (int i = 0; i < team.size(); i++) {
+            for (int j = 0; j < team.size(); j++) {
+                score += scoreBoard[team.get(i)][team.get(j)];
+            }
+        }
+        return score;
+    }
 }
