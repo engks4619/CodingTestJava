5dba03333161dacec5e47577a31ddbf8620d702a
Yechan_Lee <57943574+engks4619@users.noreply.github.com>
2024-01-03 08:09:35 +0900

    [Gold III] Title: 종이 조각, Time: 896 ms, Memory: 226724 KB -BaekjoonHub

---
+++ 백준/Gold/14391. 종이 조각/종이 조각.java
@@ -0,0 +1,80 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N, M;
+    static int[][] board;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        board = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            String str = in.readLine();
+            for (int j = 0; j < M; j++) {
+                board[i][j] = str.charAt(j) - '0';
+            }
+        }
+        int maxSum = 0;
+        Queue<boolean[][]> queue = getDividedBoardQueue();
+        while(!queue.isEmpty()){
+            maxSum = Math.max(maxSum, getSum(queue.poll()));
+        }
+        bw.write(maxSum + "\n");
+        bw.flush();
+        bw.close();
+        in.close();
+    }
+
+    static Queue<boolean[][]> getDividedBoardQueue(){
+        Queue<boolean[][]> queue = new ArrayDeque<>();
+        for (int s = 0; s < (1 << N * M); s++) {
+            boolean[][] board = new boolean[N][M];
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < M; j++) {
+                    int k = i * M + j;
+                    if((s & (1 << k)) != 0) board[i][j] = true;
+                }
+            }
+            queue.offer(board);
+        }
+
+        return queue;
+    }
+
+    static int getSum(boolean[][] selected){
+        int sum = 0;
+        for (int i = 0; i < N; i++) {
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < M; j++) {
+                if(selected[i][j]) sb.append(board[i][j]);
+                else {
+                    sum += Integer.parseInt(!sb.toString().isEmpty() ? sb.toString() : "0");
+                    sb = new StringBuilder();
+                }
+            }
+            sum += Integer.parseInt(!sb.toString().isEmpty() ? sb.toString() : "0");
+        }
+        for (int j = 0; j < M; j++) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < N; i++) {
+                if(!selected[i][j]) sb.append(board[i][j]);
+                else {
+                    sum += Integer.parseInt(!sb.toString().isEmpty() ? sb.toString() : "0");
+                    sb = new StringBuilder();
+                }
+            }
+            sum += Integer.parseInt(!sb.toString().isEmpty() ? sb.toString() : "0");
+        }
+        return sum;
+    }
+}
