8b2dd98d0fa64ed04396d8d58adf7a501295580f
Yechan_Lee <57943574+engks4619@users.noreply.github.com>
2024-01-08 23:40:14 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 268 ms, Memory: 20388 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -2,83 +2,57 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
 import java.util.function.BiFunction;
 
 public class Main {
 
     static int N, minSum = Integer.MAX_VALUE, maxSum = Integer.MIN_VALUE;
     static int[] numArr;
-    static int[] opCntArr;
-    static boolean[] visited;
-    static List<Character> opCandidateList = new ArrayList<>();
-    static char[] opArr;
+    static int[] opCntArr = new int[4];
 
     public static void main(String[] args) throws Exception {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         N = Integer.parseInt(in.readLine());
         numArr = new int[N];
-        opCntArr = new int[N - 1];
-        opArr = new char[N - 1];
-        visited = new boolean[N - 1];
         StringTokenizer st = new StringTokenizer(in.readLine());
         for (int i = 0; i < N; i++) {
             numArr[i] = Integer.parseInt(st.nextToken());
         }
         st = new StringTokenizer(in.readLine());
         for (int i = 0; i < 4; i++) {
-            int cnt = Integer.parseInt(st.nextToken());
-            while(cnt-- > 0) opCandidateList.add(operatorMap.get(i));
+            opCntArr[i] = Integer.parseInt(st.nextToken());
         }
-        go(0);
+        dfs(1, numArr[0]);
         bw.write(maxSum + "\n" + minSum + "\n");
         bw.flush();
         bw.close();
         in.close();
     }
 
-    static void go(int cnt){
-        if(cnt == N - 1){
-            updateSum();
+    static void dfs(int cnt, int sum){
+        if(cnt == N){
+            maxSum = Math.max(maxSum, sum);
+            minSum = Math.min(minSum, sum);
             return;
         }
-        for (int i = 0; i < N - 1; i++) {
-            if(visited[i]) continue;
-            opArr[cnt] = opCandidateList.get(i);
-            visited[i] = true;
-            go(cnt + 1);
-            visited[i] = false;
+        for (int i = 0; i < 4; i++) {
+            if(opCntArr[i] <= 0) continue;
+            opCntArr[i]--;
+            dfs(cnt + 1, calculateMap.get(i).apply(sum, numArr[cnt]));
+            opCntArr[i]++;
         }
     }
 
-    static void updateSum(){
-        Stack<Integer> stack = new Stack<>();
-        for (int i = N - 1; i >= 0; i--) {
-            stack.push(numArr[i]);
-        }
-        for (int i = 0; i < N - 1; i++) {
-            int tmp = calculateMap.get(opArr[i]).apply(stack.pop(), stack.pop());
-            stack.push(tmp);
-        }
-        int result = stack.pop();
-        minSum = Math.min(minSum, result);
-        maxSum = Math.max(maxSum, result);
-    }
-
-    static Map<Integer, Character> operatorMap = new HashMap<>();
+    static Map<Integer, BiFunction<Integer, Integer, Integer>> calculateMap = new HashMap<>();
     static{
-        operatorMap.put(0, '+');
-        operatorMap.put(1, '-');
-        operatorMap.put(2, '*');
-        operatorMap.put(3, '/');
+        calculateMap.put(0, (a, b)-> a + b);
+        calculateMap.put(1, (a, b)-> a - b);
+        calculateMap.put(2, (a, b)-> a * b);
+        calculateMap.put(3, (a, b)-> a / b);
     }
 
-    static Map<Character, BiFunction<Integer, Integer, Integer>> calculateMap = new HashMap<>();
-    static {
-        calculateMap.put('+', (a, b) -> a + b);
-        calculateMap.put('-', (a, b) -> a - b);
-        calculateMap.put('*', (a, b) -> a * b);
-        calculateMap.put('/', (a, b) -> a / b);
-    }
 }
