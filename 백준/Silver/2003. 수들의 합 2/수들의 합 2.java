b23bd1730d26c370f11c075bc3bdd4700f27e8b2
Yechan_Lee <57943574+engks4619@users.noreply.github.com>
2024-02-23 01:30:36 +0900

    [Silver IV] Title: 수들의 합 2, Time: 112 ms, Memory: 14444 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
@@ -6,28 +6,31 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-     public static void main(String[] args) throws Exception {
-         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-         StringTokenizer st = new StringTokenizer(in.readLine());
-         int N = Integer.parseInt(st.nextToken());
-         int M = Integer.parseInt(st.nextToken());
-         int[] arr = new int[N + 1];
-         st = new StringTokenizer(in.readLine());
-         for (int i = 1; i <= N; i++) {
-             arr[i] = Integer.parseInt(st.nextToken());
-             arr[i] += arr[i - 1];
-         }
-         int cnt = 0;
-         for (int r = N; r > 0; r--) {
-             for (int l = 0; l < r; l++) {
-                if(arr[r] - arr[l] == M) cnt++;
-             }
-         }
-         bw.write(cnt + "\n");
-         bw.flush();
-         bw.close();
-         in.close();
-     }
-     
+    public static void main(String[] args) throws Exception {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+        st = new StringTokenizer(in.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int s = 0;
+        int e = 0;
+        int sum = 0;
+        int cnt = 0;
+        while(s < N) {
+            if(sum >= M || e == N){
+                if(sum == M) cnt++;
+                sum -= arr[s++];
+            }else sum += arr[e++];
+        }
+        bw.write(cnt + "\n");
+        bw.flush();
+        bw.close();
+        in.close();
+    }
+
 }
