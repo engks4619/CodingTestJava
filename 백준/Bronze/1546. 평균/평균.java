b35d1642a153e907c2a91c017889d7fc7612d464
Yechan_Lee <57943574+engks4619@users.noreply.github.com>
2024-07-03 21:19:30 +0900

    [Bronze I] Title: 평균, Time: 84 ms, Memory: 11892 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(in.readLine());
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        int max = 0;
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            max = Math.max(max, arr[i]);
+        }
+        double sum = 0;
+        for (int i = 0; i < N; i++) {
+            sum += (double) arr[i] / max * 100;
+        }
+        System.out.println(sum / N);
+        in.close();
+    }
+
+}
