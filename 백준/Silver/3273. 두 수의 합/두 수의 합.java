f5cea1b631c8567cc14567985ed1cc13c7d428a9
Yechan_Lee <57943574+engks4619@users.noreply.github.com>
2024-07-22 01:59:10 +0900

    [Silver III] Title: 두 수의 합, Time: 268 ms, Memory: 26424 KB -BaekjoonHub

---
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(in.readLine());
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int X = Integer.parseInt(in.readLine());
+        Arrays.sort(arr);
+        int l = 0;
+        int r = N - 1;
+        int cnt = 0;
+        while(l < r) {
+            int sum = arr[l] + arr[r];
+            if(sum == X) {
+                cnt++;
+                l++;
+            }
+            else if(sum < X) l++;
+            else r--;
+        }
+        System.out.println(cnt);
+        in.close();
+    }
+    
+}
