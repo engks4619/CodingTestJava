eb8e5a55c04bd6a2e2714d8f0dadba65741718fb
Yechan_Lee <57943574+engks4619@users.noreply.github.com>
2024-11-29 20:16:23 +0900

    [Silver V] Title: 행렬 곱셈, Time: 120 ms, Memory: 14424 KB -BaekjoonHub

---
+++ 백준/Silver/2740. 행렬 곱셈/행렬 곱셈.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[][] A = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(in.readLine());
+            for (int j = 0; j < M; j++) {
+                A[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        st = new StringTokenizer(in.readLine());
+        M = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[][] B = new int[M][K];
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(in.readLine());
+            for (int j = 0; j < K; j++) {
+                B[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        int[][] C = new int[N][K];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < K; j++) {
+                for (int k = 0; k < M; k++) {
+                    C[i][j] += A[i][k] * B[k][j];
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < K; j++) {
+                sb.append(C[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
+        System.out.print(sb);
+        in.close();
+    }
+}
