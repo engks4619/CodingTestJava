106ebb933a070c24582b1284ac9e274d759eb20f
Yechan_Lee <57943574+engks4619@users.noreply.github.com>
2024-12-05 00:53:13 +0900

    [Silver V] Title: 올림픽, Time: 84 ms, Memory: 12168 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.java
+++ 백준/Silver/8979. 올림픽/올림픽.java
@@ -1,6 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -8,34 +8,30 @@ public class Main {
         StringTokenizer st = new StringTokenizer(in.readLine());
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-        List<Country> list = new ArrayList<>();
+        Country[] countries = new Country[N];
+        int target = -1;
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(in.readLine());
             int num = Integer.parseInt(st.nextToken());
             int gold = Integer.parseInt(st.nextToken());
             int silver = Integer.parseInt(st.nextToken());
             int bronze = Integer.parseInt(st.nextToken());
-            list.add(new Country(num, gold, silver, bronze));
+            countries[i] = new Country(num, gold, silver, bronze);
+            if(num == K) target = i;
         }
-        Collections.sort(list);
         int rank = 1;
-        if(list.get(0).num == K) rank = 1;
-        else {
-            for (int i = 1; i < N; i++) {
-                Country curr = list.get(i);
-                Country prev = list.get(i - 1);
-                if(prev.gold == curr.gold
-                        && prev.silver == curr.silver
-                        && prev.bronze == curr.bronze) rank = i;
-                else rank = i + 1;
-                if(curr.num == K) break;
-            }
+        Country k = countries[target];
+        for (int i = 0; i < N; i++) {
+            Country c = countries[i];
+            if(c.gold > k.gold
+                    || (c.gold == k.gold && c.silver > k.silver)
+                    || (c.gold == k.gold && c.silver == k.silver && c.bronze > k.bronze)) rank++;
         }
         System.out.println(rank);
         in.close();
     }
 
-    static class Country implements Comparable<Country> {
+    static class Country {
         int num;
         int gold;
         int silver;
@@ -47,14 +43,6 @@ public class Main {
             this.silver = silver;
             this.bronze = bronze;
         }
-
-        @Override
-        public int compareTo(Country o) {
-            if(this.gold != o.gold)
-                return o.gold - this.gold;
-            if(this.silver != o.silver)
-                return o.silver - this.silver;
-            return o.bronze - this.bronze;
-        }
     }
+
 }
